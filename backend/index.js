56e82bd2043671d8d770b67983500510823abc38
Vedant Kasar <145242784+555vedant@users.noreply.github.com>
2025-10-08 15:34:39 +0530

    fixed cors and added main page index.html

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,47 @@
 require("dotenv").config();
 const cors = require("cors");
-const express = require("express")
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoutes");
 const adminRouter = require("./routes/adminRoutes");
 const staffRouter = require("./routes/staffRoutes");
 const foodRouter = require("./routes/foodRoutes");
 const complaintRouter = require("./routes/complaintRoutes");
 
-const app = express()
+const app = express();
 app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5500",
+
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); 
-app.use('/user',userRouter);
+app.use("/uploads", express.static("uploads"));
+app.use("/user", userRouter);
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static('uploads')); 
+app.use("/uploads", express.static("uploads"));
 // app.use('/admin',adminRouter);
 // app.use('/staff',staffRouter);
-app.use('/food',foodRouter)
-app.use('/complaint', complaintRouter);
-app.get('/',(req,res)=>{
-    res.send("Server is working");
+app.use("/food", foodRouter);
+app.use("/complaint", complaintRouter);
+app.get("/", (req, res) => {
+  res.send("Server is working");
 });
 
-
 async function connect() {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to MongoDB");
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
-}   
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
 connect();
