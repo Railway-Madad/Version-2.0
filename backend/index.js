aaf8be68b4a835e55064243fc1a2ace635f0e1d7
Malve Yash Deepak <147067487+AKJ-5284@users.noreply.github.com>
2025-10-09 22:33:39 +0530

    all backend done all authentication forms done

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use("/uploads", express.static("uploads"));
 app.use("/user", userRouter);
 app.use("/uploads", express.static("uploads"));
 // app.use('/admin',adminRouter);
+app.use('/admin',adminRouter);
 app.use('/staff',staffRouter);
 app.use("/food", foodRouter);
 app.use("/complaint", complaintRouter);
