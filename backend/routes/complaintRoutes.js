1362d423af1130e74e3b37acef9dee932c0b5377
Vedant Kasar <145242784+555vedant@users.noreply.github.com>
2025-10-11 23:02:28 +0530

    API integration in Admin Dashboard

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const complaintController = require('../controllers/complaintController');
 const multer = require('multer');
 const userAuthentication = require("../middlewares/userAuthentication");
+const adminAuthentication = require('../middlewares/adminAuthentication');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -11,12 +12,12 @@ const upload = multer({ storage });
 router.get('/',userAuthentication, complaintController.getAllComplaints);
 router.post('/submit-complaint',userAuthentication, upload.single('image'), complaintController.postComplaint);
 router.get('/api/complaints/user/:username',userAuthentication, complaintController.getComplaintsByUser);
-router.put('/api/complaints/resolve/:id/',userAuthentication, complaintController.resolveComplaint);
+router.put('/api/complaints/resolve/:id/',adminAuthentication, complaintController.resolveComplaint);
 router.get('/api/complaints/:domain',userAuthentication, complaintController.getComplaintsByDomain);
 router.delete('/api/complaints/:id',userAuthentication, complaintController.deleteComplaint);
 router.get('/complaints/all',userAuthentication, complaintController.getAllComplaints);
 router.get('/api/images/user/:username',userAuthentication, complaintController.getImagesByUser);
-router.get('/api/complaintsIMP',userAuthentication, complaintController.getComplaintByStatus);
+router.get('/api/complaintsIMP',adminAuthentication, complaintController.getComplaintByStatus);
 router.get('/api/complaintsRES',userAuthentication, complaintController.getPendingComplaints);
 //reslove complaint
 
